8aa53f3d370cb2e393b4d006d1d1984b4a78601e
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-24 05:43:09 +0100

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,119 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import React, { useRef, useEffect, useState} from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function FadeInSection(props) {
+    const [isVisible, setVisible] = useState(false);
+    const domRef = useRef(null);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                console.log(`entry`, entry, `is = ${entry.isIntersecting}`);
+                setVisible(entry.isIntersecting);
+            });
+        });
+
+        const { current } = domRef;
+        observer.observe(current);
+
+        return () => observer.unobserve(current);
+    }, []);
+
+    return (
+        <div
+            className={`fade-in-section ${isVisible ? "is-visible" : ""}`}
+            ref={domRef}
+            >
+            {props.children}
+        </div>
+    );
+}
+
+function App() {
+    const [isShown, setIsShown] = React.useState(true);
+
+    return (
+        <div className='App'>
+            <button onClick={() => setIsShown(!isShown)}>
+                {isShown ? "Unmount" : "Mount"} fade in section
+            </button>
+            <h1>Fade in demo</h1>
+
+            {isShown && (
+            <>
+            <FadeInSection>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at
+                    commodo nibh, ut maximus turpis. Ut porttitor tempus augue, ac
+                    rutrum lorem rutrum eget. Donec libero lorem, luctus in maximus
+                    at, facilisis sit amet sapien. Pellentesque pharetra non urna ac
+                    porttitor. Phasellus a dignissim augue, rhoncus sagittis ante.
+                    Cras quis orci ac risus dapibus pellentesque faucibus in leo.
+                    Vivamus gravida eros a leo luctus, mollis elementum lectus
+                    viverra. Vivamus pellentesque pulvinar turpis. Cras rutrum justo
+                    at mauris aliquam, quis luctus est posuere. Quisque erat neque,
+                    sollicitudin quis mi et, commodo ultrices magna. Etiam varius
+                    efficitur elementum. Nam et lacus sit amet mi viverra lacinia.
+                    Nullam pretium est vitae tortor mollis tempor. Cras tincidunt
+                    magna eget purus gravida, et ultricies ex consequat.
+                </p>
+            </FadeInSection>
+            <FadeInSection>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at
+                    commodo nibh, ut maximus turpis. Ut porttitor tempus augue, ac
+                    rutrum lorem rutrum eget. Donec libero lorem, luctus in maximus
+                    at, facilisis sit amet sapien. Pellentesque pharetra non urna ac
+                    porttitor. Phasellus a dignissim augue, rhoncus sagittis ante.
+                    Cras quis orci ac risus dapibus pellentesque faucibus in leo.
+                    Vivamus gravida eros a leo luctus, mollis elementum lectus
+                    viverra. Vivamus pellentesque pulvinar turpis. Cras rutrum justo
+                    at mauris aliquam, quis luctus est posuere. Quisque erat neque,
+                    sollicitudin quis mi et, commodo ultrices magna. Etiam varius
+                    efficitur elementum. Nam et lacus sit amet mi viverra lacinia.
+                    Nullam pretium est vitae tortor mollis tempor. Cras tincidunt
+                    magna eget purus gravida, et ultricies ex consequat.
+                </p>
+            </FadeInSection>
+            <FadeInSection>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at
+                    commodo nibh, ut maximus turpis. Ut porttitor tempus augue, ac
+                    rutrum lorem rutrum eget. Donec libero lorem, luctus in maximus
+                    at, facilisis sit amet sapien. Pellentesque pharetra non urna ac
+                    porttitor. Phasellus a dignissim augue, rhoncus sagittis ante.
+                    Cras quis orci ac risus dapibus pellentesque faucibus in leo.
+                    Vivamus gravida eros a leo luctus, mollis elementum lectus
+                    viverra. Vivamus pellentesque pulvinar turpis. Cras rutrum justo
+                    at mauris aliquam, quis luctus est posuere. Quisque erat neque,
+                    sollicitudin quis mi et, commodo ultrices magna. Etiam varius
+                    efficitur elementum. Nam et lacus sit amet mi viverra lacinia.
+                    Nullam pretium est vitae tortor mollis tempor. Cras tincidunt
+                    magna eget purus gravida, et ultricies ex consequat.
+                </p>
+            </FadeInSection>
+            <FadeInSection>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at
+                    commodo nibh, ut maximus turpis. Ut porttitor tempus augue, ac
+                    rutrum lorem rutrum eget. Donec libero lorem, luctus in maximus
+                    at, facilisis sit amet sapien. Pellentesque pharetra non urna ac
+                    porttitor. Phasellus a dignissim augue, rhoncus sagittis ante.
+                    Cras quis orci ac risus dapibus pellentesque faucibus in leo.
+                    Vivamus gravida eros a leo luctus, mollis elementum lectus
+                    viverra. Vivamus pellentesque pulvinar turpis. Cras rutrum justo
+                    at mauris aliquam, quis luctus est posuere. Quisque erat neque,
+                    sollicitudin quis mi et, commodo ultrices magna. Etiam varius
+                    efficitur elementum. Nam et lacus sit amet mi viverra lacinia.
+                    Nullam pretium est vitae tortor mollis tempor. Cras tincidunt
+                    magna eget purus gravida, et ultricies ex consequat.
+                </p>
+            </FadeInSection>
+        </>
+    )}
     </div>
-  )
+  
+  );
 }
 
 export default App
